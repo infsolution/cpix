6b7accb510e63e9ab3d39959f4f746e2229a048e
infsolution <clsinfsolution@gmail.com>
2026-07-21 22:01:45 -0300

    Add ask to Sahre

--- src/components/FriendEmptyKeyList/index.tsx
+++ src/components/FriendEmptyKeyList/index.tsx
@@ -2,14 +2,51 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { colors } from "@/theme/colors";
-
+import { askToShare } from "@/shared/services/notification/notification.service";
+import { useAuthContext } from "@/context/auth.context";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useErrorHandler } from "@/shared/hooks/useErrorHandler";
+import { useSnackbarContext } from "@/context/snackbar.context";
+import { useState } from "react";
+import { Loading } from "../Loading";
+import { AppError } from "@/shared/helpers/AppError";
 type Props = {
   friendName: string;
+  friendId: string;
 };
-export function FriendEmptyKeyList({ friendName }: Props) {
+export function FriendEmptyKeyList({ friendName, friendId }: Props) {
+  const { user } = useAuthContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [sended, setSended] = useState(false);
+  const { handleError } = useErrorHandler();
+  const { notify } = useSnackbarContext();
+  const handleAskToShare = async () => {
+    setIsLoading(true);
+    try {
+      const { message, code, data } = await askToShare({
+        friendId: friendId,
+        headings: "Compartilhar Chave",
+        contents: `${user?.name} pede que você compartilhe ou cadastre suas chaves PIX com ele!`,
+        data: { solicitante: user?.name },
+        deepLink: "xavex://profile",
+      });
+      if (code != "200") {
+        throw new AppError(message);
+      }
+      notify({
+        message: "Solicitação enviada com sucesso.",
+        messageType: "SUCCESS",
+      });
+      setSended(true);
+    } catch (error) {
+      handleError(error, "Erro na solicitação");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <View style={styles.container}>
-      <FontAwesome6 name="pix" size={92} color={colors.green.btn} />
+      <FontAwesome6 name="pix" size={68} color={colors.green.btn} />
       <Text style={styles.titleMain}>OPA!</Text>
       <View style={styles.info}>
         <Text style={styles.title}>
@@ -19,17 +56,32 @@ export function FriendEmptyKeyList({ friendName }: Props) {
           Peça para que cadastre ou compartilhe uma chave!
         </Text>
       </View>
-      <TouchableOpacity
-        onPress={() => console.log("Pedir para compartilha")}
-        activeOpacity={0.8}
-      >
-        <FontAwesome6
-          name="slideshare"
-          size={48}
-          color={colors.list.capitular}
-        />
-      </TouchableOpacity>
-      <Text style={styles.text}>Pedir para compartilhar!</Text>
+      {!isLoading && !sended && (
+        <View style={styles.askToShare}>
+          <TouchableOpacity onPress={handleAskToShare} activeOpacity={0.8}>
+            <FontAwesome6
+              name="slideshare"
+              size={48}
+              color={colors.list.capitular}
+            />
+          </TouchableOpacity>
+          <Text style={styles.text}>Pedir para compartilhar!</Text>
+        </View>
+      )}
+
+      {!isLoading && sended && (
+        <View style={styles.askToShare}>
+          <TouchableOpacity activeOpacity={0.8}>
+            <MaterialCommunityIcons
+              name="transit-connection-variant"
+              size={48}
+              color={colors.list.capitular}
+            />
+          </TouchableOpacity>
+          <Text style={styles.text}>Solicitação enviada</Text>
+        </View>
+      )}
+      {isLoading && <Loading />}
     </View>
   );
 }
--- src/components/FriendEmptyKeyList/styles.ts
+++ src/components/FriendEmptyKeyList/styles.ts
@@ -2,28 +2,31 @@ import { StyleSheet } from "react-native";
 import { colors } from "@/theme/colors";
 
 export const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        borderRadius: 24,
-        backgroundColor: colors.list.circle,
-        alignItems: "center",
-        padding: 12
-    },
-    info: {
-        justifyContent: "flex-start",
-        marginBottom: 24
-    },
-    text: {
-        fontSize: 14,
-        color: colors.text.label
-    },
-    title: {
-        fontSize: 16,
-        color: colors.text.label
-    },
-    titleMain: {
-        fontSize: 22,
-        fontWeight: "bold",
-        color: colors.text.label
-    }
+  container: {
+    width: "100%",
+    borderRadius: 24,
+    backgroundColor: colors.list.circle,
+    alignItems: "center",
+    padding: 8,
+  },
+  info: {
+    justifyContent: "flex-start",
+    marginBottom: 24,
+  },
+  text: {
+    fontSize: 14,
+    color: colors.text.label,
+  },
+  title: {
+    fontSize: 16,
+    color: colors.text.label,
+  },
+  titleMain: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: colors.text.label,
+  },
+  askToShare: {
+    alignItems: "center",
+  },
 });
